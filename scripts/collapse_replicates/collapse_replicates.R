b6016f59b04f2cbf6ccaefbaa59c175c14a99b33
YuhJong Liu <yuhjong24@gmail.com>
2026-05-21 11:29:07 -0400

    Added cell set flag

--- scripts/collapse_replicates/collapse_replicates.R
+++ scripts/collapse_replicates/collapse_replicates.R
@@ -47,38 +47,35 @@ write_out_table(collapsed_l2fc, collapsed_l2fc_outpath)
 
 if (args$mt_filter) {
   message("Monotonicity QC filter: On")
+  # Set up lists of column names for inputs
   trt_cl_cols = unique(c(setdiff(sig_cols, c("pert_dose", "pert_dose_unit", "pert2_dose", "pert2_dose_unit")),
                          cell_line_cols))
   trt_pool_cols = unique(c(sig_cols, intersect(cell_line_cols, c("cell_set", "pool_id"))))
-  outlier_pool = get_monotonicity(collapsed_l2fc, trt_cl_cols = trt_cl_cols, trt_pool_cols = trt_pool_cols)
-
-  # Get sig cols with cell set
   if ("cell_set" %in% sig_cols) {
-    grouping_cols = sig_cols
+    trt_cell_set_cols = sig_cols
   } else {
     messsage("Attempting to add cell_set to sig_cols ...")
     if ("cell_set" %in% names(collapsed_l2fc)) {
-      grouping_cols = c(sig_cols, "cell_set")
+      trt_cell_set_cols = c(sig_cols, "cell_set")
     } else {
       stop("Cannot find cell_set column.")
     }
   }
 
-  # Flag pert + cell sets with lots of flagged pools
-  outlier_pool = outlier_pool |>
-    dplyr::group_by(across(all_of(grouping_cols))) |>
-    dplyr::mutate(flag_pert = sum(outlier == TRUE) / dplyr::n() > 0.5,
-                  pool_flag = dplyr::case_when(outlier == TRUE ~ "Poor pool nonotonicity",
-                                               flag_pert == TRUE ~ "Several pools in pert failed monotonicity",
-                                               .default = NA))
+  monotonicity = get_monotonicity(collapsed_l2fc, trt_cl_cols)
+  flagged_trt_pools = flag_breaks(monotonicity, trt_pool_cols, trt_cell_set_cols, pool_cutoff = 0.25)
 
-  # Note dropped perts
-  filtered_perts = outlier_pool |>
-    dplyr::filter(flag_pert == TRUE) |>
-    dplyr::distinct(across(all_of(grouping_cols)))
-  message("The following ", nrow(filtered_perts),
-          " pert + cell_set were dropped due to a large number of pools breaking monotonicity.")
-  print(filtered_perts)
+  # Check if any cell sets were dropped
+  failed_cell_sets = flagged_trt_pools |>
+    dplyr::filter(cell_set_pass_ratio == 0) |>
+    dplyr::distinct(dplyr::across(tidyselect::all_of(trt_cell_set_cols)))
+
+  # Print out failing cell sets
+  if (nrow(failed_cell_sets) > 0) {
+    message("The following ", nrow(failed_cell_sets),
+            " cell_sets were dropped due to a large number of pools breaking monotonicity.")
+    print(failed_cell_sets)
+  }
 
   # Create a qc output directory if one does not exist
   if (!dir.exists(file.path(args$out, "qc_tables"))) {
@@ -87,9 +84,9 @@ if (args$mt_filter) {
   # Write out outlier pool table as a csv
   outlier_pool_outpath = file.path(args$out, "qc_tables", "outlier_trt_pools.csv")
   message("Writing out monotonicity filter file to ", outlier_pool_outpath)
-  write_out_table(outlier_pool, outlier_pool_outpath)
+  write_out_table(flagged_trt_pools, outlier_pool_outpath)
 
-  failed_pools = outlier_pool |> dplyr::filter(outlier == TRUE)
+  failed_pools = flagged_trt_pools |> dplyr::filter(!is.na(cell_set_flag))
   message("Filtering out ", nrow(failed_pools), " pools from collapsed_l2fc")
   join_cols = c("pert_plate", "pert_name", "pert_dose", "cell_set", "pool_id")
   collapsed_l2fc = collapsed_l2fc |> dplyr::anti_join(failed_pools, by = join_cols)
