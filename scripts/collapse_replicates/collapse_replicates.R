c69fdfed8269a5d5c67cd5a427cfbbb3c118284b
YuhJong Liu <yuhjong24@gmail.com>
2026-05-27 13:49:33 -0400

    Added comments

--- scripts/collapse_replicates/collapse_replicates.R
+++ scripts/collapse_replicates/collapse_replicates.R
@@ -25,7 +25,6 @@ parser$add_argument("--collapsed_l2fc_file", default = "collapsed_l2fc.csv",
 parser$add_argument("--mt_filter", type = "logical", default = TRUE, help = "Filter out outlier treatment pools")
 parser$add_argument("-o", "--out", default = getwd(), help = "Output path. Default is working directory")
 
-
 # get command line options, if help option encountered print help and exit
 args <- parser$parse_args()
 
@@ -45,12 +44,17 @@ collapsed_l2fc_outpath = file.path(args$out, "collapsed_l2fc_original.csv")
 message("Writing out initial collapsed l2fc file to ", collapsed_l2fc_outpath)
 write_out_table(collapsed_l2fc, collapsed_l2fc_outpath)
 
-if (args$mt_filter) {
+if (args$mt_filter == TRUE) {
   message("Monotonicity QC filter: On")
   # Set up lists of column names for inputs
   trt_cl_cols = unique(c(setdiff(sig_cols, c("pert_dose", "pert_dose_unit", "pert2_dose", "pert2_dose_unit")),
                          cell_line_cols))
+  # trt_cl_cols are used to group all doses of a perturbation for a cell line together
   trt_pool_cols = unique(c(sig_cols, intersect(cell_line_cols, c("cell_set", "pool_id"))))
+  # trt_pool_cols are used to group all lines of a pool in each perturbation together
+
+  # trt_cell_set_cols are used to group all lines of a cell set in each perturbation together
+  # "cell_set" may already exist in "sig_cols", so only add "cell_set" if it is missing
   if ("cell_set" %in% sig_cols) {
     trt_cell_set_cols = sig_cols
   } else {
@@ -58,19 +62,21 @@ if (args$mt_filter) {
     if ("cell_set" %in% names(collapsed_l2fc)) {
       trt_cell_set_cols = c(sig_cols, "cell_set")
     } else {
-      stop("Cannot find cell_set column.")
+      # Error out if "cell_set is not present"
+      stop("Cannot find cell_set column in collapsed_l2fc - check sig_cols and/or cell_line_cols!")
     }
   }
 
+  # Call monotonicity check and flag treatment pools
   monotonicity = get_monotonicity(collapsed_l2fc, trt_cl_cols)
   flagged_trt_pools = flag_breaks(monotonicity, trt_pool_cols, trt_cell_set_cols, pool_cutoff = 0.25)
 
-  # Check if any cell sets were dropped
+  # Check if any pert + cell sets were flagged
   failed_cell_sets = flagged_trt_pools |>
     dplyr::filter(cell_set_pass_ratio < 0.5) |>
     dplyr::distinct(dplyr::across(tidyselect::all_of(trt_cell_set_cols)))
 
-  # Print out failing cell sets
+  # Print out flagged pert + cell sets
   if (nrow(failed_cell_sets) > 0) {
     message("The following ", nrow(failed_cell_sets),
             " cell_sets were dropped due to a large number of pools breaking monotonicity.")
@@ -81,11 +87,13 @@ if (args$mt_filter) {
   if (!dir.exists(file.path(args$out, "qc_tables"))) {
     dir.create(file.path(args$out, "qc_tables"))
   }
-  # Write out outlier pool table as a csv
+
+  # Write out treatment pool qc table as a csv
   outlier_pool_outpath = file.path(args$out, "qc_tables", "trt_pools_qc.csv")
   message("Writing out monotonicity filter file to ", outlier_pool_outpath)
   write_out_table(flagged_trt_pools, outlier_pool_outpath)
 
+  # Filter out failing pools from collapsed_l2fc
   failed_pools = flagged_trt_pools |> dplyr::filter(!is.na(cell_set_flag))
   message("Filtering out ", nrow(failed_pools), " pools from collapsed_l2fc")
   collapsed_l2fc = collapsed_l2fc |> dplyr::anti_join(failed_pools, by = trt_pool_cols)
